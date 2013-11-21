9b33187c2ff91f34b283bcf2fffb598f9fd9c035
Cindy Leschaud <cindy.leschaud@gmail.com>
2013-11-21 15:44:08 +0100

    matrimonial homepage

--- app/views/layouts/matrimonial.blade.php
+++ app/views/layouts/matrimonial.blade.php
@@ -24,20 +24,50 @@
     		
 		        <header id="header" class="inner"> 
 		        	<div class="row">
-		        		<h1 class="large-4 columns"><a class="" href="">{{HTML::image('/images/matrimonial/logo.png')}}</a></h1>
+		        		<h1 class="large-4 columns noPadding"><a class="" href="">{{HTML::image('/images/matrimonial/logo.png')}}</a></h1>
 						<nav class="large-8 columns" id="menu-principal">
 						   <a href="<?php //echo action('AppController@getIndex');?>">Home</a>
 						   <a href="<?php //echo action('AppController@getIndex');?>">Newsletter</a>
 						   <a href="<?php //echo action('AppController@getIndex');?>">Jurisprudence</a>
 						</nav>
 		        	</div>
-			    </header>  
+			    </header> 
+			     
+			    <section id="photo">{{HTML::image('/images/matrimonial/photo.jpg')}}</section>
+			    
+		      	<section id="breadcrumbs" class="greySection inner">Home <a href=""> &gt; Newsletter</a></section>
 		      	
 	            @yield('content')
 	            
+	            <footer class="greySection inner">
+		            © 2013 - droitmatrimonial.ch<br/>
+					Université de Neuchâtel, Faculté de droit, Av. du 1er mars 26, 2000 Neuchâtel<br/>
+					<a href="mailto:droit.matrimonial@unine,ch">droit.matrimonial(at)unine(dot)ch</a>
+	            </footer>
+	            
             </div>  
-            <div id="sidebar" class="inner">	
-            	{{HTML::image('/images/matrimonial/unine.png')}}
+            <div id="sidebar">	
+            	<p class="inner">{{HTML::image('/images/matrimonial/unine.png')}}</p>
+            	
+            	<div class="pinkBlock inner">
+	            	<h5>Inscription à la newsletter</h5>
+					<p>Entrez votre adresse e-mail</p>
+					
+					{{ Form::open(array( 'url' => 'newsletter', 'class' => '')) }}						
+						{{ Form::text('email', '' , array('class' => '')) }}
+						{{ Form::submit('Valider', array('class' => 'button tiny')) }}
+					{{ Form::close() }}
+					
+            	</div>
+            	
+            	<div class="inner">
+            	
+            		{{ Form::open(array( 'url' => 'search', 'class' => 'searchform')) }}						
+				        {{ Form::text('email', '' , array('class' => '')) }}
+						{{ Form::submit('ok', array('class' => '')) }}
+				    {{ Form::close() }}
+				     
+            	</div>
             </div>  
 			<div class="clearall"></div>
 	    </div>
--- app/views/matrimonial/index.blade.php
+++ app/views/matrimonial/index.blade.php
@@ -3,7 +3,36 @@
 @section('content')
 	
     <div id="content" class="inner">
-      Matrimonial index
+	    <div class="row">
+			<div class="large-12 columns">	
+				<h1>Droit matrimonial</h1>
+				<p><strong>Se tenir informé en quelques clics</strong></p>
+				<p>Ce site, lancé sous l'égide de la Faculté de droit de l'Université de Neuchâtel par les prof. François Bohnet et Olivier Guillod et Me Sabrina Burgat, docteure en droit, 
+				est dédié aux nouveautés en droit matrimonial et aux décisions du Tribunal fédéral dans ce domaine. Il permet de prendre connaissance rapidement des derniers arrêts 
+				rendus et de retrouver toute la jurisprudence en droit matrimonial depuis 2011, organisée par chapitres.</p>
+				
+				<h5 class="line">News</h5>
+			</div>
+		</div>		
+    	<!-- arret block -->
+		<div class="row">
+			<div class="large-4 columns">
+				<h4>TF 5A_352/2013 - commenté par François Bohnet</h4>
+				<p>L'auteur se penche sur la problématique des frais judiciaires et dépens en matière de divorce et vous propose une analyse de l’arrêt du Tribunal fédéral 5A_352/2013 du 22 août 2013. </p>
+				<p><a href="">En savoir plus</a></p>
+			</div>
+			<div class="large-4 columns">
+				<h4>TF 5A_352/2013 - commenté par François Bohnet</h4>
+				<p>L'auteur se penche sur la problématique des frais judiciaires et dépens en matière de divorce et vous propose une analyse de l’arrêt du Tribunal fédéral 5A_352/2013 du 22 août 2013. </p>
+				<p><a href="">En savoir plus</a></p>	
+			</div>
+			<div class="large-4 columns">
+				<h4>TF 5A_352/2013 - commenté par François Bohnet</h4>
+				<p>L'auteur se penche sur la problématique des frais judiciaires et dépens en matière de divorce et vous propose une analyse de l’arrêt du Tribunal fédéral 5A_352/2013 du 22 août 2013. </p>
+				<p><a href="">En savoir plus</a></p>
+			</div>
+		</div>
+		
     </div>
-	
+		
 @stop
